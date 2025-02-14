4aaf6fcb779591936480c8b878529103fc70931b
Aaditya <abilak@gmail.com>
2025-02-14 01:28:07 -0500

    added some functions and rough code
    the function is incomplete – will work on later. Lots to be done on data preprocessing

---
+++ cnn_timeseries.py
@@ -0,0 +1,2 @@
+# use hybrid models; that is, train one model on the time series. then train another modle on the residuals. 
+# same kaggle link
--- preprocess_pipeline.py
+++ preprocess_pipeline.py
@@ -1,4 +1,9 @@
-# function to preprocess data for CNN and LSTM (may requrie different steps, if so, make diff functions for each)
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.feature_selection import SelectKBest, chi2
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+# function to preprocess data for CNN and LSTM (may require different steps, if so, make diff functions for each)
 
 '''
 FROM THE README OF THE DATASET (MAY BE HELPFUL):
@@ -20,3 +25,44 @@ Notes:
 	2) Degrees are measured from 0 to 360. Since 0 and 360 represent the same spot on a circle, consider transforming these using sine and/or cosine. Also consider converting them to radians, instead of degrees.
 	3) Each location can have a different model. There is no reason to build one model to work for all locations.
 '''
+'''
+SEASONAL FEATURES - temperature_2m, dewpoint_2m
+'''
+
+# Lots of data preprocessing to be done - fourier features, lag features (partial autocorr), time dummies, etc.
+# good place to look: https://www.kaggle.com/learn/time-series. not great tbh to learn models, but good for data processing
+
+df = pd.read_csv('data/Location1.csv')
+feature = 'windgusts_10m'
+df = df[['Time', feature]]
+df['Time'] = pd.to_datetime(df['Time'])
+df.set_index('Time', inplace=True)
+df = df.resample('W').mean()
+plt.figure(figsize=(10, 6))
+plt.plot(df.index, df[feature], marker='o', linestyle='-', color='b')
+plt.title('Timestamp vs Target Variable')
+plt.xlabel('Timestamp')
+plt.ylabel('Target')
+plt.xticks(rotation=45)
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+def preprocess_data_cnn(data, feature_selection = 5): 
+    '''Input as csv'''
+    data = data.drop('Timestamp', axis = 1) # we
+    data['winddirection_10m'] = np.cos(data['winddirection_10m'] * np.pi / 180) # maybe get rid of cosine transformation
+    data['winddirection_100m'] = np.cos(data['winddirection_100m'] * np.pi / 180)
+    target = data['Power']
+    rest = data.drop('Power')
+    # normalize
+    scaler = MinMaxScaler()
+    cols = target.columns 
+    rest = scaler.fit_transform(rest)
+    rest = pd.DataFrame(rest, columns = cols)
+    
+    # select best features 
+    select_k_best = SelectKBest(score_func=chi2, k=feature_selection)
+    rest = select_k_best.fit_transform(rest, target)
+    
+    
+    
